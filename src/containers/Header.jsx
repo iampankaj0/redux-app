73295ab397600a7099d3b89254dacf9dc6eaffcf
Pankaj Yadav <91059277+iampankaj0@users.noreply.github.com>
2022-05-30 09:28:37 +0530

    ..

--- src/containers/Header.jsx
+++ src/containers/Header.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
@@ -15,4 +15,4 @@ const Header = () => {
   )
 }
 
-export default Header
+export default Header;
